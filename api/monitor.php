48c20a05d608b637e16a3d6298b37fb559de4172
Florian Häußinger <floderflo@gmx.de>
2019-07-21 13:51:17 +0200

    Entity-Monitor nur für admin

--- api/monitor.php
+++ api/monitor.php
@@ -1,16 +1,35 @@
 <?php
+use PHPMailer\PHPMailer\Exception;
 
 include_once __DIR__ . '/../api.php';
+include_once __DIR__ . '/../vendor/ADFC-Hamburg/flexapi/requestutils/jwt.php';
 
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Credentials");
 
-FlexAPI::guard()->login([
-    'username' => 'guest',
-    'password' => ''
-]);
 
-$history = FlexAPI::get('entityMonitor')->history($_GET['entity'],$_GET['id']);
+try {
+    $jwt = getJWT();
+    if ($jwt) {
+        FlexAPI::guard()->login($jwt);
+    }
 
-echo json_encode($history);
+    if (FlexAPI::guard()->getUsername() === 'admin') {
+        $history = FlexAPI::get('entityMonitor')->history($_GET['entity'],$_GET['id']);
+    } else {
+        throw(new Exception('Not allowed.', 403));
+    }
+    $response = [
+        'code' => 200,
+        'message' => 'History reconstructed.',
+        'history' => $history
+    ];
+} catch (Exception $exc) {
+    $response = [
+        'code' => $exc->getCode(),
+        'message' => 'Could not reconstruct history: '.$exc->getMessage()
+    ];
+}
+
+echo json_encode($response);
