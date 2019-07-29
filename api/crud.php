183c0f4beb98e1672ca28905a4d020abe79cc7a2
Sven Anders <sven@anders-hamburg.de>
2019-07-29 14:30:53 +0200

    allow-header in crud.php

--- api/crud.php
+++ api/crud.php
@@ -5,7 +5,6 @@ include_once __DIR__ . '/../vendor/ADFC-Hamburg/flexapi/requestutils/jwt.php';
 
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Credentials");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Authorization, X-Requested-With, Credentials");
 
 echo FlexAPI::crud();
-
