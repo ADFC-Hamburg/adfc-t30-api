2411d3356a31a06a0ad3e83616353a3ebfd01f8c
Sven Anders <sven@anders-hamburg.de>
2019-07-29 14:12:36 +0200

    allow-credentials-header

--- api/portal.php
+++ api/portal.php
@@ -4,7 +4,6 @@ include_once __DIR__ . '/../api.php';
 
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
-// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 echo FlexAPI::portal();
