b0b2279807d29b10d427618fe1ca0159365fbcee
Sven Anders <sven@anders-hamburg.de>
2019-07-29 14:40:15 +0200

    allow-methods

--- api/portal.php
+++ api/portal.php
@@ -5,5 +5,6 @@ include_once __DIR__ . '/../api.php';
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, PUT, POST, PATCH, DELETE, OPTIONS, HEAD");
 
 echo FlexAPI::portal();
