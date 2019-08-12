0f11dab2bd1c47d8acb1135443512f608c34fc23
Sven Anders <sven@anders-hamburg.de>
2019-08-12 08:13:11 +0200

    extended deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,20 +12,20 @@ function get_secret {
 
 mkdir -p $T30_SEC_DIR
 
+VERSION=$(jq -r '.version' composer.json)
 MYSQL_ROOT_PW="$(cat ~/${T30_SEC_DIR}/mysql.root.secret)"
 T30_PW=$(get_secret t30.db)
 T30_ADMIN=$(get_secret t30.admin)
+DATABASE="t30paten_$(echo $OLD_VERSION |sed -e 's/\./_/g')"
 
 echo "== Drop Database =="
-echo "DROP DATABASE IF EXISTS t30; " | mysql -u root -p"${MYSQL_ROOT_PW}"
-echo "DROP USER IF EXISTS 't30'@'localhost'; " | mysql -u root -p"${MYSQL_ROOT_PW}"
-
+echo "DROP DATABASE IF EXISTS $DATABASE; " | mysql -u root -p"${MYSQL_ROOT_PW}"
 
 echo "== Create Database =="
 mysql -u root -p"${MYSQL_ROOT_PW}" <<EOF
-CREATE DATABASE t30;
-CREATE USER 't30'@'localhost' IDENTIFIED BY '$T30_PW';
-GRANT ALL PRIVILEGES ON t30.* TO 't30'@'localhost';
+CREATE DATABASE ${DATABASE};
+CREATE USER IF NOT EXISTS 't30'@'localhost' IDENTIFIED BY '$T30_PW';
+GRANT ALL PRIVILEGES ON ${DATABASE}.* TO 't30'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
@@ -33,8 +33,8 @@ echo "== Create Config File =="
 cp api.conf.example.php api.conf.php
 sed -i -e "s/t30-db-password/${T30_PW}/" api.conf.php
 sed -i -e "s/t30-db-user/t30/" api.conf.php
-sed -i -e "s/t30-db-name/t30/" api.conf.php
+sed -i -e "s/t30-db-name/${DATABASE}/" api.conf.php
 
 echo 'Call setup.php'
 
-curl -v -H "Content-Type: application/json" -d '{ "resetSecret": "IBs1G38VUCiH6HEIlMrqXEGXkpaq9JKy", "adminPassword": "${T30_ADMIN}", "fillInTestData": true, "registerTestUser": true}'  http:/127.0.0.1:1234/setup.php
+curl -v -H "Content-Type: application/json" -d '{ "resetSecret": "IBs1G38VUCiH6HEIlMrqXEGXkpaq9JKy", "adminPassword": "${T30_ADMIN}", "fillInTestData": true, "registerTestUser": true}'  "https://tools.adfc-hamburg.de/t30-paten/api/version${VERSION}/setup.php"
