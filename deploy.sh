2c0f5e723b2f3a67bac404bf9f69899252341e0e
Sven Anders <sven@anders-hamburg.de>
2019-09-25 14:07:43 +0200

    Deploy: add geodaten, andere adresse für changes

--- deploy.sh
+++ deploy.sh
@@ -57,9 +57,11 @@ sed -i -e "s/adfc@ben-steffen.de/tempo30sozial@hamburg.adfc.de/" api.conf.php
 sed -i -e "s/http/https/" api.conf.php
 sed -i -e "s/ADFC Hamburg/ADFC Hamburg Tempo30 vor sozialen Einrichtungen/" api.conf.php
 sed -i -e "s/\/adfc\/api-2019-07\/adfc-t30-api/\/t30-paten\/api\/version${VERSION}/" api.conf.php
-sed -i -e 's/"projekt-leiterin-t30@adfc-hamburg.de", "system-admin-t30@adfc-hamburg.de"/ "t30changes2019@sven.anders.hamburg" /' api.conf.php
+sed -i -e 's/"projekt-leiterin-t30@adfc-hamburg.de", "system-admin-t30@adfc-hamburg.de"/ "t30-changes@hamburg.adfc.de" /' api.conf.php
 composer install
 
 echo 'Call setup.php'
 
 curl -v -H "Content-Type: application/json" -d '{ "resetSecret": "IBs1G38VUCiH6HEIlMrqXEGXkpaq9JKy", "adminPassword": "'"${T30_ADMIN}"'", "fillInTestData": true, "registerTestUser": true}'  "https://tools.adfc-hamburg.de/t30-paten/api/version${VERSION}/setup.php"
+
+mysql "-u${DATABASE}" "-p${T30_PW}" "${DATABASE}" < /var/www/html/t30-paten/daten/geodaten.sql
