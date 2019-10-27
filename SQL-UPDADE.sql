ce0f781aad1700f36e6f65b7439b9ad70533cd00
Sven Anders <sven@anders-hamburg.de>
2019-10-27 10:10:45 +0100

    Fix SQL Statement

--- SQL-UPDADE.sql
+++ SQL-UPDADE.sql
@@ -3,4 +3,4 @@ ALTER TABLE demandedstreetsection MODIFY house_no_to VARCHAR(255);
 ALTER TABLE demandedstreetsection DROP other_streets_checked;
 INSERT INTO crudpermission (role,entity,`read`) VALUES ("guest","email",1);
 DELETE FROM crudpermission WHERE role="admin" and entity="email" and `create`=0;
-ALTER TABLE company ADD company int(11) NOT NULL DEFAULT 0;
+ALTER TABLE Institution ADD company int(11) NOT NULL DEFAULT 0;
