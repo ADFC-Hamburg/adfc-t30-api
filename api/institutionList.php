571982a90d04309d9fc517a3966c9c9968b81005
Sven Anders <sven@anders-hamburg.de>
2019-10-28 14:42:51 +0100

    Entferne Warnung:  Use of undefined constant x - assumed x

--- api/institutionList.php
+++ api/institutionList.php
@@ -9,7 +9,7 @@ $pdo = new PDO('mysql:host='.$dbcfg['host'].';dbname='.$dbcfg['database'], $dbcf
 
 $statement = $pdo->prepare("SELECT i.id,i.status,i.name, i.street_house_no, i.zip, i.city, i.type, ST_X(i.position) as x, ST_Y(i.position) as y, t.district FROM `institution` i,  `township` t WHERE  ST_CONTAINS(`t`.`geom`,`i`.`position`)");
 function map_func($row) {
-  $row['position']=[$row[x], $row['y']];
+  $row['position']=[$row['x'], $row['y']];
   unset($row['x']);
   unset($row['y']);
   return $row;
